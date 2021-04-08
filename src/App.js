c0b81559622528c78af3f2a6314e2815856eab15
Cecilio Cofler <ccofler@gmail.com>
2021-04-08 06:51:32 -0700

    Shop page and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+//import logo from './logo.svg';
 import './App.css';
-import HomePage from './pages/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header   from './components/header/header.component';
 
 function App() {
   return (
     <div>
-        <HomePage/>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route  path='/shop' component={ShopPage} />
+        </Switch>
+
     </div>
   );
 }
