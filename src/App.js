54d0dd5f41dddddb92f8bb96044e3be1339155b8
Cecilio Cofler <ccofler@gmail.com>
2021-04-13 14:50:32 -0700

    Adding cart and redux objects

--- src/App.js
+++ src/App.js
@@ -1,70 +1,73 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-//import logo from './logo.svg';
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-signup.components';
-import Header   from './components/header/header.component';
-import { auth ,createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser  } from './redux/user/user.actions';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-
-
-  unsubscribefFromAuth = null;
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const  { setCurrentUser } = this.props;
+    const { setCurrentUser } = this.props;
 
-    this.unsubscribefFromAuth = auth.onAuthStateChanged(async userAuth => {
-          if ( userAuth) {
-            const userRef = await createUserProfileDocument(userAuth);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-            //console.log("USER REF:", userRef);
-            userRef.onSnapshot(snapShot =>  {
-              setCurrentUser({
-                  id: snapShot.id,
-                  ...snapShot.data()
-              })
-              //console.log("CHANGES IN STATE",this.state);
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      }
 
-            });
-        }
-        
-        
-        setCurrentUser(userAuth);
-
-        //createUserProfileDocument(user);
-        //console.log(user);
+      setCurrentUser(userAuth);
     });
-
   }
 
   componentWillUnmount() {
-    this.unsubscribefFromAuth();
+    this.unsubscribeFromAuth();
   }
 
   render() {
     return (
-        <div>
-            <Header />
-            <Switch>
-              <Route exact path='/' component={HomePage} />
-              <Route exact path='/estore' component={HomePage} />
-              <Route  path='/shop' component={ShopPage} />
-              <Route  path='/signin' component={SignInAndSignUpPage} />
-            </Switch>
-
-        </div>
-      );
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ?
+                 ( <Redirect to='/' /> ) : (<SignInAndSignUpPage />
+              )
+            }
+          />
+        </Switch>
+      </div>
+    );
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser : user => dispatch(setCurrentUser(user))
-})
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
 
-export default connect(null ,  mapDispatchToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
-import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
 
 
 import store from './redux/store';
+
+import './index.css';
+import App from './App';
+
+
+
+
 
 //import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 //import reportWebVitals from './reportWebVitals';
