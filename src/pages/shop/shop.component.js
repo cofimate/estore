bd4e69c5eb6b85eba282d4b8d14575e584e3ede3
Cecilio Cofler <ccofler@gmail.com>
2021-04-14 18:10:25 -0700

    Adding category with navigation

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,29 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shop.data';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+//        /*{ collections.map(({id , ...oherCollectionProps }) => 
 
-class ShopPage extends React.Component {
+const ShopPage = ({ match }) =>  (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+);
 
-    constructor(props) {
-        super(props)
-
-        this.state =  {
-            collections : SHOP_DATA
-        };
-    }
-
-
-    render() { 
-        const { collections } = this.state;
-        return (<div className='shop-page'>
-                { collections.map(({id , ...oherCollectionProps }) => (
-                        <CollectionPreview key={id} {...oherCollectionProps} />
-                    ))}
-                </div>
-            );
-    }
-}
 
 export default ShopPage;
